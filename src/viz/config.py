55d7344643b680a78b825a9cb25bcdc1cd909d74
Sayan Goswami <goswami.sayan47@gmail.com>
2018-04-12 10:24:00 +0530

    Viz - initial commit

---
+++ src/viz/config.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+
+PROCESSED_DATA = "../data/processedData.pkl"
---
+++ src/viz/main.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+from collections import Counter
+from config import *
+import pickle
