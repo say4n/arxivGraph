9699e8134d3a99a562146f847b7d3b1e6636a75a
Sayan Goswami <goswami.sayan47@gmail.com>
2018-04-12 12:19:32 +0530

    Viz network - WIP

--- src/viz/main.py
+++ src/viz/main.py
@@ -2,4 +2,45 @@
 
 from collections import Counter
 from config import *
+from flask import Flask
+from flask import request, render_template
+import json
 import pickle
+
+
+with open(PROCESSED_DATA, "rb") as fp:
+    exported_data = pickle.load(fp)
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+
+
+@app.route("/")
+def index():
+    pairs_sieved = exported_data["pairs_sieved"]
+    authors_from_sieved_pairs = exported_data["authors_from_sieved_pairs"]
+
+    author_list = list(map(str, authors_from_sieved_pairs.keys()))
+
+    nodes = list(map(lambda author: {
+                        "name": author
+                     },
+                     author_list))
+
+    edges = list(map(lambda link: {
+                         "source"    : str(author_list.index(link[0])),
+                         "target"    : str(author_list.index(link[1])),
+                         "value"     : pairs_sieved[link]
+                     },
+                     pairs_sieved.keys()))
+
+    nodelist = json.dumps({
+        "nodes" : nodes,
+        "edges" : edges
+    })
+
+    return render_template('index.html', nodelist=nodelist)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
